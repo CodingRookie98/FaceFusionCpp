f6a23eb491723a9c91a4d171e8c2d78bc28a005e
CodingRookie98 <520whjoker@gmail.com>
2025-02-08 14:28:50 +0800

    remove custom triplets for vcpkg

--- cmake/vcpkg_custom_triplets/clang/x64-windows.cmake
+++
@@ -1,9 +0,0 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-set(VCPKG_PLATFORM_TOOLSET "C:/devTools/msys64/clang64")
-
-set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -stdlib=libc++")
-set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} ")
-message(STATUS "Custom triplet for clang: VCPKG_CXX_FLAGS -> ${VCPKG_CXX_FLAGS}")
-message(STATUS "Custom triplet for clang: VCPKG_C_FLAGS -> ${VCPKG_C_FLAGS}")
