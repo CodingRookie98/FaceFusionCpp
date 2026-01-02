2e8d338d833339b53e9f5b940682fd445c7b0527
CodingRookie98 <520whjoker@gmail.com>
2026-01-02 11:27:35 +0800

    feat(任务管理): 添加任务管理模块实现任务队列和任务处理功能
    实现任务管理模块的核心功能，包括：
    - 任务提交和队列管理
    - 任务预处理和合法性检查
    - 任务执行线程管理
    - 线程安全的队列操作
    - 与核心模块的交互接口

---
+++ facefusionCpp/src/task_manager.cpp
@@ -0,0 +1,184 @@
+/**
+ ******************************************************************************
+ * @file           : task_manager.cpp
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2025/12/26
+ ******************************************************************************
+ */
+
+module;
+#include <mutex>
+#include <utility>
+#include <boost/process.hpp>
+
+module task_manager;
+import utils;
+import file_system;
+import processor_hub;
+import model_manager;
+
+namespace ffc::task_manager {
+
+using namespace model_manager;
+
+TaskManager& TaskManager::get_instance() {
+    static TaskManager instance;
+    static std::once_flag flag;
+    std::call_once(flag, [&]() {
+        instance.m_logger->trace("TaskManager instance created.");
+        auto thread = std::jthread([&]() {
+            instance.run_tasks();
+        });
+        thread.detach();
+    });
+    return instance;
+}
+
+void TaskManager::set_core(std::shared_ptr<Core> core) {
+    m_core = std::move(core);
+    m_logger->trace("TaskManager.core set.");
+}
+
+std::string TaskManager::get_current_task_id() {
+    std::lock_guard lock_current_task_id(m_mutex_current_task_id);
+    return m_current_task_id;
+}
+
+std::string TaskManager::submit_task(Task task) {
+    std::string task_id;
+    if (prepare_task(task)) {
+        task_id = utils::generate_uuid();
+    }
+    if (task_id.empty()) {
+        return task_id;
+    }
+
+    {
+        std::unique_lock w_lock_task_map(m_mutex_task_map);
+        m_task_map[task_id] = task;
+    }
+
+    {
+        std::unique_lock w_lock_task_queue(m_mutex_task_queue);
+        m_task_queue.push(task_id);
+    }
+
+    return task_id;
+}
+
+bool TaskManager::prepare_task(Task& task) {
+    if (task.target_paths.empty()) {
+        m_logger->error("TaskManager.prepare_task: target_paths is empty.");
+        return false;
+    }
+    if (task.output.path.empty()) {
+        task.output.path = file_system::get_current_path();
+    } else {
+        if (!file_system::is_dir(task.output.path)) {
+            m_logger->warn("TaskManager.prepare_task: output.path is not a directory. Replace with default output directory.");
+            task.output.path = file_system::get_current_path();
+        }
+    }
+
+    std::vector<std::string> target_paths;
+    for (const auto& path : task.target_paths) {
+        if (file_system::is_file(path)) {
+            if (file_system::is_image(path) || file_system::is_video(path)) {
+                target_paths.push_back(path);
+            }
+        } else {
+            auto files = file_system::list_files(path);
+            target_paths.insert(target_paths.end(), files.begin(), files.end());
+        }
+    }
+    task.target_paths = std::move(target_paths);
+
+    for (auto& [type, model, parameters] : task.processors_info) {
+        if (type == ProcessorMajorType::FaceSwapper) {
+            if (!ModelManager::is_face_swapper_model(model)) {
+                m_logger->warn(
+                    std::format("TaskManager.prepare_task: Face swapper model ({}) not found. Replace with default model ({}).",
+                                ModelManager::get_instance()->get_model_name(model),
+                                ModelManager::get_instance()->get_model_name(model_manager::Model::Inswapper_128_fp16)));
+                model = model_manager::Model::Inswapper_128_fp16;
+            } else {
+            }
+        } else if (type == ProcessorMajorType::FaceEnhancer) {
+            if (!ModelManager::is_face_enhancer_model(model)) {
+                m_logger->warn(
+                    std::format("TaskManager.prepare_task: Face enhancer model ({}) not found. Replace with default model ({}).",
+                                ModelManager::get_instance()->get_model_name(model),
+                                ModelManager::get_instance()->get_model_name(model_manager::Model::Codeformer)));
+                model = model_manager::Model::Codeformer;
+            } else {
+                if (!parameters.contains("blend_factor")) {
+                    m_logger->warn(std::format("TaskManager.prepare_task: Face enhancer model ({}) blend_factor not found. Replace with default value (0.8).",
+                                               ModelManager::get_instance()->get_model_name(model)));
+                    parameters["blend_factor"] = "0.8";
+                }
+            }
+        } else if (type == ProcessorMajorType::ExpressionRestorer) {
+            // Todo
+            // if (!ModelManager::is_expression_restorer_model(model)) {
+            //     logger_->warn(
+            //         std::format("TaskManager.prepare_task: Expression restorer model ({}) not found. Replace with default model ({}).",
+            //                     ModelManager::get_model_name(model),
+            //                     ModelManager::get_model_name(model_manager::Model::Gfpgan_14)));
+            //     model = model_manager::Model::Gfpgan_14;
+            // }
+        }
+    }
+
+    return true;
+}
+
+void TaskManager::run_tasks() {
+    while (true) {
+        {
+            std::unique_lock lock_current_task_id(m_mutex_current_task_id);
+            m_current_task_id.clear();
+        }
+
+        std::string task_id;
+        {
+            bool is_task_queue_empty = false;
+            {
+                std::lock_guard lock_task_queue(m_mutex_task_queue);
+                if (m_task_queue.empty()) {
+                    is_task_queue_empty = true;
+                }
+            }
+
+            if (is_task_queue_empty) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                continue;
+            } else {
+                std::lock_guard lock_task_queue(m_mutex_task_queue);
+                task_id = m_task_queue.front();
+                m_task_queue.pop();
+            }
+        }
+        if (task_id.empty()) {
+            continue;
+        }
+
+        Task task;
+        {
+            std::lock_guard lock_task_map(m_mutex_task_map);
+            task = m_task_map[task_id];
+        }
+        {
+            std::lock_guard lock_current_task_id(m_mutex_current_task_id);
+            m_current_task_id = task_id;
+        }
+        // Todo 接收任务结果，更新任务状态
+        m_core->run_task(task);
+        {
+            std::lock_guard lock_task_map(m_mutex_task_map);
+            m_task_map.erase(task_id);
+        }
+    }
+}
+} // namespace ffc::task_manager
---
+++ facefusionCpp/src/task_manager.ixx
@@ -0,0 +1,61 @@
+/**
+ ******************************************************************************
+ * @file           : task_manager.ixx
+ * @author         : CodingRookie
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2025/12/26
+ ******************************************************************************
+ */
+
+module;
+#include <mutex>
+#include <queue>
+#include <string>
+#include <unordered_map>
+
+export module task_manager;
+import task;
+import logger;
+import core;
+
+namespace ffc::task_manager {
+class TaskManager {
+public:
+    TaskManager(const TaskManager&) = delete;
+    TaskManager& operator=(const TaskManager&) = delete;
+
+    static TaskManager& get_instance();
+    void set_core(std::shared_ptr<Core> core);
+    // 提交任务到任务队列, 返回任务ID。成功返回任务ID，失败返回空字符串。
+    std::string submit_task(Task task);
+    // 获取当前正在运行的任务ID。如果没有任务在运行，则返回空字符串。
+    std::string get_current_task_id();
+
+private:
+    TaskManager() = default;
+    ~TaskManager() = default;
+
+    // 准备任务, 并且检查任务是否合法, 合法则返回true, 否则返回false。
+    bool prepare_task(Task& task);
+    // 运行任务队列中的任务。每次运行一个任务，就会从任务队列中取出一个任务，并运行。
+    // 这个函数会在单独的线程中运行, 直到程序退出时才会结束。
+    [[noreturn]] void run_tasks();
+    std::shared_ptr<Logger> m_logger = Logger::get_instance();
+    std::shared_ptr<Core> m_core;
+    // 键为任务ID, 值为任务对象。
+    std::unordered_map<std::string, Task> m_task_map;
+    std::mutex m_mutex_task_map;
+    // 任务队列, 用于存储待运行的任务。
+    std::queue<std::string> m_task_queue;
+    /**
+     * @brief 任务队列互斥锁
+     *
+     * 用于保护任务队列的线程安全访问。
+     */
+    std::mutex m_mutex_task_queue;
+    // 当前正在运行的任务ID。如果没有任务在运行，则为空字符串。
+    std::string m_current_task_id;
+    std::mutex m_mutex_current_task_id;
+};
+} // namespace ffc::task_manager
