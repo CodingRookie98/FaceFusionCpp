59e1627c1cb8931bb3caf19427b5962f24227e37
CodingRookie98 <520whjoker@gmail.com>
2026-01-04 21:10:02 +0800

    refactor(main): 重构主程序结构并更新依赖项
    移除旧的ini配置系统，添加yaml和json支持
    注释掉旧的核心运行逻辑，为新的配置系统做准备
    更新命名空间和函数调用以符合新的代码规范

--- facefusionCpp/main.cpp
+++ facefusionCpp/main.cpp
@@ -2,45 +2,53 @@
 #ifdef _WIN32
 #include <Windows.h>
 #endif
+#include <yaml-cpp/yaml.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 import processor_hub;
 import core;
-import ini_config;
-import ini_config;
+import core_options;
 import logger;
 import metadata;
 import file_system;
-import cli_config;
+import utils;
+import task;
+import serialize;
 
 using namespace ffc;
+using namespace ffc::core;
+using namespace ffc::infra;
+using namespace ffc::ai::model_manager;
+
 
 int main(int argc, char** argv) {
-    FileSystem::setLocalToUTF8();
+    file_system::set_local_to_utf8();
 
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
-    const std::string tmpPath = FileSystem::getTempPath() + "/" + metadata::name;
-    FileSystem::removeDir(tmpPath);
+    // test_task();
+    // test_config();
 
-    ini_config ini_config;
-    ini_config.loadConfig();
+    // from_json(config_json, core_options);
 
-    auto coreOptions = ini_config.getCoreOptions();
-    auto coreTask = ini_config.getCoreRunOptions();
-
-    CliConfig cliConfig;
-    if (!cliConfig.parse(argc, argv, coreOptions, coreTask)) {
-        return 0;
-    }
-
-    const auto core = std::make_shared<ffc::Core>(coreOptions);
-    const bool ok = core->Run(coreTask);
-
-    if (!ok) {
-        Logger::getInstance()->error("FaceFusionCpp failed to run. Maybe some of the tasks failed.");
-    }
-    FileSystem::removeDir(tmpPath);
+    // const std::string tmpPath = FileSystem::get_temp_path() + "/" + metadata::name;
+    // FileSystem::remove_dir(tmpPath);
+    //
+    // ini_config ini_config;
+    // ini_config.loadConfig();
+    //
+    // auto coreOptions = ini_config.getCoreOptions();
+    // auto coreTask = ini_config.getCoreRunOptions();
+    //
+    // const auto core = std::make_shared<ffc::Core>(coreOptions);
+    // const bool ok = core->Run(coreTask);
+    //
+    // if (!ok) {
+    //     Logger::getInstance()->error("FaceFusionCpp failed to run. Maybe some of the tasks failed.");
+    // }
+    // FileSystem::remove_dir(tmpPath);
     return 0;
 }
