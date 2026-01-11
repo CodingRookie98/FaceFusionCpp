0066d558ee4c604c876e518641323955f9ce23ea
CodingRookie98 <520whjoker@gmail.com>
2026-01-11 22:18:46 +0800

    refactor(build): 重构CMake模块系统以简化库创建
    将模块创建功能从modules.cmake迁移到modules_lib.cmake，并增强其功能
    更新所有相关CMakeLists.txt文件以使用新的add_modules_library函数

---
+++ cmake/modules_lib.cmake
@@ -0,0 +1,19 @@
+function(add_modules_library TARGET_NAME)
+    add_library(${TARGET_NAME} ${ARGN}) # Support STATIC/SHARED arguments
+
+    target_compile_features(${TARGET_NAME} PUBLIC cxx_std_20)
+
+    set_target_properties(${TARGET_NAME} PROPERTIES
+        CXX_STANDARD 20
+        CXX_STANDARD_REQUIRED YES
+        CXX_EXTENSIONS OFF
+    )
+
+    if (MSVC)
+        target_compile_options(${TARGET_NAME} PRIVATE
+            /permissive-
+            /bigobj
+            /wd5050
+        )
+    endif()
+endfunction()
