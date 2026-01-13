7338be989d9e76c02ed163861feb743dd5294e3c
CodingRookie98 <520whjoker@gmail.com>
2026-01-14 01:15:36 +0800

    feat: implement FairFace classification logic

---
+++ src/domain/face/classifier/impl/fair_face.cpp
@@ -0,0 +1,121 @@
+module;
+#include <opencv2/opencv.hpp>
+#include <onnxruntime_cxx_api.h>
+
+module domain.face.classifier;
+import :fair_face;
+import domain.face;
+import domain.face.helper;
+
+namespace domain::face::classifier {
+
+FairFace::FairFace() = default;
+
+void FairFace::load_model(const std::string& model_path,
+                          const foundation::ai::inference_session::Options& options) {
+    InferenceSession::load_model(model_path, options);
+    auto input_dims = get_input_node_dims();
+    m_inputWidth = static_cast<int>(input_dims[0][2]);
+    m_inputHeight = static_cast<int>(input_dims[0][3]);
+    m_size = cv::Size(m_inputWidth, m_inputHeight);
+}
+
+std::vector<float> FairFace::getInputImageData(
+    const cv::Mat& image, const domain::face::types::Landmarks& face_landmark_5) const {
+    cv::Mat inputImage;
+    std::tie(inputImage, std::ignore) = helper::warp_face_by_face_landmarks_5(
+        image, face_landmark_5, helper::get_warp_template(m_WarpTemplateType), m_size);
+
+    std::vector<cv::Mat> inputChannels(3);
+    cv::split(inputImage, inputChannels);
+    for (int c = 0; c < 3; c++) {
+        inputChannels[c].convertTo(inputChannels[c], CV_32FC1,
+                                   1 / (255.0 * m_standardDeviation.at(c)),
+                                   -m_mean.at(c) / m_standardDeviation.at(c));
+    }
+
+    const int imageArea = inputImage.cols * inputImage.rows;
+    std::vector<float> inputData(imageArea * 3);
+    size_t singleChannelSize = imageArea * sizeof(float);
+    memcpy(inputData.data(), reinterpret_cast<float*>(inputChannels[2].data),
+           singleChannelSize); // R
+    memcpy(inputData.data() + imageArea, reinterpret_cast<float*>(inputChannels[1].data),
+           singleChannelSize); // G
+    memcpy(inputData.data() + 2 * imageArea, reinterpret_cast<float*>(inputChannels[0].data),
+           singleChannelSize); // B
+    return inputData;
+}
+
+ClassificationResult FairFace::classify(const cv::Mat& image,
+                                        const domain::face::types::Landmarks& face_landmark_5) {
+    std::vector<float> inputData = getInputImageData(image, face_landmark_5);
+    std::vector<int64_t> inputShape{1, 3, m_inputHeight, m_inputWidth};
+
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+    std::vector<Ort::Value> inputTensor;
+    inputTensor.emplace_back(Ort::Value::CreateTensor<float>(
+        memory_info, inputData.data(), inputData.size(), inputShape.data(), inputShape.size()));
+
+    std::vector<Ort::Value> outputTensor = run(inputTensor);
+
+    int64_t raceId = outputTensor[0].GetTensorMutableData<int64_t>()[0];
+    int64_t genderId = outputTensor[1].GetTensorMutableData<int64_t>()[0];
+    int64_t ageId = outputTensor[2].GetTensorMutableData<int64_t>()[0];
+
+    ClassificationResult result{};
+    result.age = categorizeAge(ageId);
+    result.gender = categorizeGender(genderId);
+    result.race = categorizeRace(raceId);
+    return result;
+}
+
+domain::face::AgeRange FairFace::categorizeAge(int64_t age_id) {
+    domain::face::AgeRange age{};
+
+    if (age_id == 0) {
+        age.min = 0;
+        age.max = 2;
+    } else if (age_id == 1) {
+        age.min = 3;
+        age.max = 9;
+    } else if (age_id == 2) {
+        age.min = 10;
+        age.max = 19;
+    } else if (age_id == 3) {
+        age.min = 20;
+        age.max = 29;
+    } else if (age_id == 4) {
+        age.min = 30;
+        age.max = 39;
+    } else if (age_id == 5) {
+        age.min = 40;
+        age.max = 49;
+    } else if (age_id == 6) {
+        age.min = 50;
+        age.max = 59;
+    } else if (age_id == 7) {
+        age.min = 60;
+        age.max = 69;
+    } else {
+        age.min = 70;
+        age.max = 100;
+    }
+
+    return age;
+}
+
+domain::face::Gender FairFace::categorizeGender(int64_t genderId) {
+    if (genderId == 0) { return domain::face::Gender::Male; }
+    return domain::face::Gender::Female;
+}
+
+domain::face::Race FairFace::categorizeRace(int64_t raceId) {
+    if (raceId == 1) { return domain::face::Race::Black; }
+    if (raceId == 2) { return domain::face::Race::Latino; }
+    if (raceId == 3 || raceId == 4) { return domain::face::Race::Asian; }
+    if (raceId == 5) { return domain::face::Race::Indian; }
+    if (raceId == 6) { return domain::face::Race::Arabic; }
+    return domain::face::Race::White;
+}
+
+} // namespace domain::face::classifier
